b8c5852362cbe8196a138677044e8ecedb7c2370
musekwa <musekwa2011@gmail.com>
2024-03-22 12:54:00 +0200

    Fixed 4

--- src/components/selector-icon.tsx
+++ src/components/selector-icon.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-export const SelectorIcon = (props) => (
+export const SelectorIcon = (props: any) => (
     <svg
       aria-hidden="true"
       fill="none"
